046234f5142f6877bedfc203aa8b4359e7322b94
mradrian <mr.podkalicki.adrian94@gmail.com>
2020-02-19 23:20:47 +0100

    set connect with MongoDB

--- server/index.js
+++ server/index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
+require('./mongooseDB/coonectToDB');
 
-mongoose.connect();
 
 
 
--- server/mongooseDB/coonectToDB.js
+++ server/mongooseDB/coonectToDB.js
@@ -1,2 +1,23 @@
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+
+
+function connectToDB() {
+    try {
+         mongoose.connect(keys.mongoURI, {
+             useNewUrlParser: true,
+             useUnifiedTopology: true,
+        });
+
+            console.log('Connected to MongoDB...');
+    } catch (err) {
+        console.error('Connection failed...', err);
+    }
+}
+connectToDB()
+
+
+
 
 
