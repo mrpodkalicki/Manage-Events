d6e0dbbe35aaf4e822ed469843a4c06716984f56
mradrian <mr.podkalicki.adrian94@gmail.com>
2020-02-21 00:12:12 +0100

    add Class Event, add createEvent method

--- server/mongooseDB/models/EventForm.js
+++ server/mongooseDB/models/EventForm.js
@@ -1,6 +1,27 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
+class Event {
+    constructor( firstName, lastName, email, date_created, tags  ) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.date_created = date_created;
+        this.tags = tags;
+
+    }
+    creatEvent(EventModal){
+        return new EventModal({
+            firstName : this.firstName,
+            lastName : this.lastName,
+            email : this.email,
+            date_created : this.date_created,
+            tags : this.tags
+        })
+    }
+}
+
+
 const eventSchema = new Schema({
     firstName : {
         type: String,
@@ -30,4 +51,11 @@ const eventSchema = new Schema({
         maxlength: 512
     }
 });
-const Event = mongoose.model('Event', eventSchema);
+
+const EventModal = mongoose.model('Event', eventSchema);
+
+const eventOne = new Event("adi", "pppp", "adi@gmail", "20-02-20", "#js#react#redux");
+
+module.exports.eventOne = eventOne;
+module.exports.EventModal = EventModal;
+
