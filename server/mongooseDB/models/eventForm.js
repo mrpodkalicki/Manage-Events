33e1c3f3fdd9b03f9b474436b9cf6f6a584abcec
mradrian <mr.podkalicki.adrian94@gmail.com>
2020-02-21 23:58:44 +0100

    change name EventForm to eventForm

---
+++ server/mongooseDB/models/eventForm.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+
+const eventSchema = new Schema({
+    title: {
+        type: String,
+        required:true,
+        minlength: 6,
+        maxlength: 64
+    },
+    person: {
+        type: mongoose.ObjectId,
+        required: true,
+    },
+    Data: {
+        startHour: {
+            type : String,
+            minlength: 1,
+            maxlength: 5
+        },
+        endHour: {
+            type: String,
+            minlength: 1,
+            maxlength: 5
+        },
+        dateWhen: {
+            type: String,
+            required: true,
+        },
+        dateCreateEvent: {
+            type:Date,
+            required: true,
+        }
+    },
+    tags: {
+        type: String,
+        required: true,
+        minlength: 15,
+        maxlength: 512
+    }
+});
+
+const Event = mongoose.model('Event', eventSchema);
+module.exports.Event = Event;
+
