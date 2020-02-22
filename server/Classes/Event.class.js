dea499f6be50700dbb421a860bc8a2f864a42b5b
mrpodkalicki <mr.podkalicki.adrian94@gmail.com>
2020-02-22 20:24:49 +0100

    change inheritance in Event Class, change eventData on dataInformation, add properties:IfExistinDB, idObjInDb

--- server/Classes/Event.class.js
+++ server/Classes/Event.class.js
@@ -1,24 +1,30 @@
 const  Person  = require('../Classes/Person.class');
 
-class Event extends Person{
-    constructor( title, personId, eventData, tags ){
-        super();
+class Event {
+    constructor( title, personId, dataInformation, tags ){
+
         this.title = title;
         this.personId = personId;
-        this.eventData = eventData;
+        this.dataInformation = dataInformation;
         this.tags = tags;
+        this.IfExistinDB = false;
+        this.idObjInDb = {};
     };
     async creatEvent(EventModel){
         return await new EventModel({
             title: this.title,
             personId: this.personId,
-            dataInformation: {
-                startHour:  this.eventData.startHour,
-                endHour:    this.eventData.endHour,
-                dateWhen:   this.eventData.dateWhen,
+            dataInformation:  {
+                startHour:  this.dataInformation.startHour,
+                endHour:    this.dataInformation.endHour,
+                dateWhen:   this.dataInformation.dateWhen,
             },
             tags : this.tags
         });
     };
 }
+Event.prototype.checkIfExistInDB = Person.prototype.checkIfExistInDB;
+Event.prototype.saveToDb = Person.prototype.saveToDb;
+
+
 module.exports = Event;
