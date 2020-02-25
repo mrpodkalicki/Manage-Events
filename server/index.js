4c2ca140cf52d1b043c220541808a1357a3a3998
mrpodkalicki <mr.podkalicki.adrian94@gmail.com>
2020-02-25 23:17:26 +0100

    add  route path, add methods: bodyParser and express.json

--- server/index.js
+++ server/index.js
@@ -1,19 +1,27 @@
-"use strict"
+"use strict";
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const connectToMongoDB = require('./mongooseDB/coonectToDB');
-const eventRoute = require('./routes/events.routes')
 
-
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+app.use(bodyParser.json());
 
 connectToMongoDB(
     ).then(
         console.log('Connected to MongoDB')
-    ).catch(err => {
+    )
+    .catch(err => {
         console.log(err)
 });
 
-eventRoute()
+app.use('/events',require('./routes/showEvents.route'));
+app.use('/createEvent',require('./routes/createEvent.routes'));
+
+
 
 
 const PORT  = process.env.PORT || 5000;
